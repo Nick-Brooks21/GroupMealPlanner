25649ef4a155eaa5d0ad492e828c516affd131bc
mposs93 <54368675+mposs93@users.noreply.github.com>
2019-11-09 13:20:46 -0600

    added a few more html pages
    added some pages and get/post methods. Currently only the index page and inputMeal page are working (though you cannot submit a new meal yet)

--- src/main/java/dmacc/controller/WebController.java
+++ src/main/java/dmacc/controller/WebController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import dmacc.beans.Meal;
 import dmacc.repository.MealPlannerRepository;
 
 @Controller
@@ -13,8 +16,22 @@ public class WebController {
 	@Autowired
 	MealPlannerRepository repo;
 	
-	@GetMapping("/viewAll")
+	@GetMapping("/viewAllMeals")
 	public String viewAllMeals(Model model) {
+		model.addAttribute("mealPlanner", repo.findAll());
+		return "results";
+	}
+	
+	@GetMapping("/inputMeal")
+	public String addNewMeal(Model model) {
+	    Meal m = new Meal();
+	    model.addAttribute("newMeal", m);
+	    return "inputMeal";
+	}
+	
+	@PostMapping("/inputMeal")
+	public String addNewMeal(@ModelAttribute Meal m, Model model) {
+		repo.save(m);
 		model.addAttribute("mealPlanner", repo.findAll());
 		return "results";
 	}
