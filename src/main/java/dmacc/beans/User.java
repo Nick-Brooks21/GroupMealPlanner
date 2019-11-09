f166bfb08b0031fea29e1f7237c97b7b5f89c3db
AngusTheBeef <54658992+AngusTheBeef@users.noreply.github.com>
2019-11-08 22:00:22 -0700

    user class + index.html and html template
    user class + 2 html files(which don't do anything yet)

---
+++ src/main/java/dmacc/beans/User.java
@@ -0,0 +1,49 @@
+package dmacc.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long userid;
+	private String userName;
+	
+	public User() {
+		super();
+	}
+	
+	public User(String userName) {
+		super();
+		this.userName = userName;
+	}
+
+	public long getUserid() {
+		return userid;
+	}
+
+	public void setUserid(long userid) {
+		this.userid = userid;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userid=" + userid + ", userName=" + userName + "]";
+	}
+	
+	
+	
+	
+}
--- src/main/java/dmacc/controller/WebController.java
+++ src/main/java/dmacc/controller/WebController.java
@@ -1,5 +1,14 @@
 package dmacc.controller;
 
-public class WebController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import dmacc.beans.Ingredients;
+import dmacc.repository.MealPlannerRepository;
+//just some import statements for later
+public class WebController {
+	
 }
